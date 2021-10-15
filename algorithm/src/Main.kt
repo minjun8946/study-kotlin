d3e264ad9774bc3cfca6de21d81ec54c95ef949d
minjun8946 <minjun8946@daum.net>
2021-10-15 20:05:03 +0900

    👨🏻‍💻:: main

---
+++ algorithm/src/Main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val a = intArrayOf(95, 90, 99, 99, 80, 99)
+    val b = intArrayOf(1,1,1,1,1,1)
+    val c = intArrayOf(93,33,55)
+    val d = intArrayOf(1,30,5)
+    //developFun(a,b)
+    //println(quickSort(listOf(1,3,3,4,5,6,25,134513,451345,4,56)))
+    math(500)
+}
